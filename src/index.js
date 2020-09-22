6cb35d5cb44486237a6b8007718a45071dfc6575
diego gonçalves <jaksleydiego@gmail.com>
2020-09-22 00:41:15 -0400

    first  commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Teste from '~/teste';
+
+// import { Container } from './styles';
+
+export default function src() {
+  return (
+    <View>
+      <Teste />
+    </View>
+  );
+}
